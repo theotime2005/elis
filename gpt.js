97707e8beb03ea316600caf0f7ea2187de0d96eb
theotime2005 <theotime.berthod@epitech.digital>
2024-05-03 11:49:48 +0200

    On utilise un fetch au lieu du module

--- gpt.js
+++ gpt.js
@@ -1,19 +1,34 @@
-const OpenAI = require('openai')
-
-const openai = new OpenAI({
-    apiKey: process.env.GPT_KEY
-});
-
 async function main(question) {
-    const completion = await openai.chat.completions.create({
-        max_tokens: 150,
-        messages: [{ role: "system", content: "Tu es un bot discord qui est sur un serveur. Tes réponses ne doives pas être injurieuses ou vulgaires. Tu dois t'adresser à la personne en lui disant vous. Pour finir, si le terme pain au chocolat viens dans la conversation, tu devras lui répondre la phrase suivante: On dit Chocolatine!!!" },
-            {role: "user", content: question}],
-        model: "gpt-4",
-    });
-
-    const response = completion.choices[0];
-    return response.message.content;
+    try {
+        const request = await fetch("https://api.openai.com/v1/chat/completions", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${process.env.GPT_KEY}`
+            },
+            body: JSON.stringify({
+                "model": "gpt-3.5-turbo",
+                "messages": [
+                    {
+                        "role": "system",
+                        "content": "Tu es un bot discord qui est sur un serveur. Tes réponses ne doives pas être injurieuses ou vulgaires. Tu dois t'adresser à la personne en lui disant vous. Pour finir, si le terme pain au chocolat viens dans la conversation, tu devras lui répondre la phrase suivante: On dit Chocolatine!!!"
+                    },
+                    {
+                        "role": "user",
+                        "content": question
+                    }
+                ],
+                "temperature": 0.7
+            })
+        });
+        if (request.status===200) {
+            const response = await request.json();
+            return response.choices[0].message.content;
+        }
+    } catch (error) {
+        console.error(error);
+        return "Une erreur s'est produite, je n'ai pas réussi à te répondre!";
+    }
 }
 
 exports.ask_gpt=main;
